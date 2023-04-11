da3655506bf4d6e7267db7d71ac3686e6409e6bb
lacydeth <57276660+lacydeth@users.noreply.github.com>
2023-04-12 00:44:28 +0200

    Delete html directory

--- html/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    define("DB_HOST","localhost");
-    define("DB_USER","WEB");
-    define("DB_PASSWORD","xYzPass01");
-    define("DB_DATABASE","versenydata");
-
-    $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE);
-
-?>
